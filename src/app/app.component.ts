1333302db950cf4218c4a1380a69aa033aa351be
Jorge Oliveira <jorge.juniortwo@hotmail.com>
2024-11-30 06:33:08 -0300

    Terminando css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AuthService } from './authentication/services/auth.service'; // Caminho corrigido
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  standalone: true,  // Tornando o componente standalone
-  imports: [RouterOutlet],  // Importando módulos necessários aqui
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule, // Importando MatIconModule
+    MatButtonModule, // Importando MatButtonModule
+    MatInputModule // Importando MatInputModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularFlix';
-
-  constructor(private authService: AuthService) {}
-
-  logout() {
-    this.authService.logout();
-  }
-}
+export class AppComponent { }
